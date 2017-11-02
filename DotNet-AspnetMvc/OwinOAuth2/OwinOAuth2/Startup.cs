f4afba2e648ae64e65d65b2b3fbccd1e539f09de
林云琦(linyunqi) <linyunqi@tuhu.cn>
2017-11-02 16:22:56 +0800

    OwinOAuth2

---
+++ DotNet-AspnetMvc/OwinOAuth2/OwinOAuth2/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinOAuth2.Startup))]
+
+namespace OwinOAuth2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
